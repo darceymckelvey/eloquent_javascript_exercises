5373aa64afee8e07b3a11ed8f6bacc7cb33345e4
Darcey Mckelvey <darceymckelvey@gmail.com>
2019-01-21 23:29:24 -0500

    added comments to FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,14 @@
+
+/*
+ * ELOQUENT JAVASCRIPT - CHAPTER 2 - EXERCISE 2
+ * FIZZBUZZ
+ *
+ * Write a program that uses console.log to print all the numbers from 1 to 100
+ * with two exceptions. For numbers divisible by 3 print "Fizz", for numbers
+ * divisible by 5 print "Buzz", divisible by both print "FizzBuzz".
+ *
+ * */
+
 var counter = 1;
 
 while (counter <= 100) {
@@ -15,3 +26,15 @@ while (counter <= 100) {
         counter++;
     }
 }
+
+/*
+ * The solution at https://eloquentjavascript.net/code/#2.2
+ * 
+ * for (let n = 1; n <= 100; n++) {
+ *     let output = "";
+ *     if (n % 3 == 0) output += "Fizz";
+ *     if (n % 5 == 0) output += "Buzz";
+ *     console.log(output || n);
+ * }
+ *
+ * */
